483ca7951749edc7238fde4292432d2a626bddab
MarionQH <marion.huet49@gmail.com>
2025-04-24 15:54:08 +0200

    instalation tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './resources/views/**/*.edge',
+    './resources/js/**/*.js',
+    './resources/css/**/*.css',
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+};
