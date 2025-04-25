1b1034e8446d5be92e5dba21c297f73ee60665b9
MarionQH <marion.huet49@gmail.com>
2025-04-25 12:17:15 +0200

    homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
 // tailwind.config.js
 module.exports = {
-  content: [
-    './resources/views/**/*.edge',
-    './resources/js/**/*.js',
-    './resources/css/**/*.css',
-  ],
+  content: ['./resources/views/**/*.edge', './resources/js/**/*.js', './resources/css/**/*.css'],
   theme: {
     extend: {
     },
   },
   plugins: [],
-};
+}
